4552cade466bf6fce09382ad1f55c935e32f30d5
Lucy Norman <lucynorman@mac.com>
2024-10-19 09:56:13 +0000

    remember to do git commits!

---
+++ Assets/js/script.js
@@ -0,0 +1,57 @@
+const API_KEY = "QcYWHleKGfeULnzU53i93n6SWoo";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+// A function to make the request and a function to display the data
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+                    "Authorization": API_KEY,
+                 },
+                 body: form,
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            console.log(data);
+        } else {
+            throw new Error(data.error);
+        }
+}
+
+// make a GET request to the API URL with the API key, pass the data to a display function
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if(response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
+
+
+
